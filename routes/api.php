b825bd6a229c3fd68a0d949075a202e87387dca8
unknown <alamin.php100@gmail.com>
2022-11-22 02:13:30 +0600

    added jwt auth

--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,19 @@ Route::apiResource('/class',ClassController::class);
 Route::apiResource('/subject',SubjectController::class);
 Route::apiResource('/section',SectionController::class);
 Route::apiResource('/student',StudentController::class);
+
+Route::group([
+    'namespace' => 'App\Http\Controllers',
+    'prefix' => 'auth'
+
+], function () {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+    Route::post('payload', 'AuthController@payload');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('register', 'AuthController@register');
+
+});
